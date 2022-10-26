ad54dd3a539b8c33ab2b98d1e31d104c3085187a
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-10-26 10:03:25 +0200

    add class ProduktSpeicher

---
+++ src/datenspeicherung/ProduktSpeicher.java
@@ -0,0 +1,31 @@
+package datenspeicherung;
+
+import java.util.ArrayList;
+
+public class ProduktSpeicher
+{
+	public ProduktSpeicher()
+	{
+		
+	}
+	
+	public void aendere(int pProduktNr, String pBezeichnung, double pVerkaufspreis, double pLagerbestand)
+	{
+		
+	}
+	
+	public void fuegeHinzu(String pBezeichnung, double pVerkaufspreis, double pLagerbestand)
+	{
+		
+	}
+	
+	public ArrayList<ProduktDaten> liesProdukte()
+	{
+		
+	}
+	
+	public void loesche(int pProduktNr)
+	{
+		
+	}
+}
