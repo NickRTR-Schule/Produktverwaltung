28be028cc2f55898965ae9f648914adbf7c0e6a2
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-01-25 11:00:07 +0100

    Datenbankmethoden in Steuerung einbinden

--- src/steuerung/Steuerung.java
+++ src/steuerung/Steuerung.java
@@ -6,31 +6,51 @@ import datenspeicherung.db.ProduktSpeicher;
 public class Steuerung 
 {
 	private ProduktSpeicher speicher;
+	private Benutzerschnittstelle benutzerschnittstelle;
 	
 	public Steuerung(Benutzerschnittstelle pBenutzerschnittstelle)
 	{
+		benutzerschnittstelle = pBenutzerschnittstelle;
 		speicher = new ProduktSpeicher();
 		try {
-			pBenutzerschnittstelle.zeigeProdukte(speicher.liesProdukte());
+			benutzerschnittstelle.zeigeProdukte(speicher.liesProdukte());
 		} catch (Exception e) {
 			e.printStackTrace();
-			//gui.zeigeMeldung(e.getLocalizedMessage());
+			benutzerschnittstelle.zeigeMeldung(e.getLocalizedMessage());
 		}
 	}
 	
 	public void geklicktAendern(int pProduktNr, String pBezeichnung, double pVerkaufspreis, double pLagerbestand)
 	{
-		speicher.aendere(pProduktNr, pBezeichnung, pVerkaufspreis, pLagerbestand);
+		try {
+			speicher.aendere(pProduktNr, pBezeichnung, pVerkaufspreis, pLagerbestand);
+			benutzerschnittstelle.zeigeProdukte(speicher.liesProdukte());
+		} catch (Exception e) {
+			e.printStackTrace();
+			benutzerschnittstelle.zeigeMeldung(e.getLocalizedMessage());
+		}
 	}
 	
 	public void geklicktHinzufuegen(String pBezeichnung, double pVerkaufspreis, double pLagerbestand)
 	{
-		speicher.fuegeHinzu(pBezeichnung, pVerkaufspreis, pLagerbestand);
+		try {
+			speicher.fuegeHinzu(pBezeichnung, pVerkaufspreis, pLagerbestand);
+			benutzerschnittstelle.zeigeProdukte(speicher.liesProdukte());
+		} catch (Exception e) {
+			e.printStackTrace();
+			benutzerschnittstelle.zeigeMeldung(e.getLocalizedMessage());
+		}
 	}	
 
 	public void geklicktLoeschen(int pProduktNr)
 	{
-		speicher.loesche(pProduktNr);
+		try {
+			speicher.loesche(pProduktNr);
+			benutzerschnittstelle.zeigeProdukte(speicher.liesProdukte());
+		} catch (Exception e) {
+			e.printStackTrace();
+			benutzerschnittstelle.zeigeMeldung(e.getLocalizedMessage());
+		}
 	}
 
 }
